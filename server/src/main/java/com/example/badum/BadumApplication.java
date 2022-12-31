5284b101dc6c2118ec2a11be2781892d185eca9c
Ceïf <cecece775@gmail.com>
2022-12-31 19:34:43 +0100

    Backend initialisation

---
+++ server/src/main/java/com/example/badum/BadumApplication.java
@@ -0,0 +1,13 @@
+package com.example.badum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadumApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/badum/BadumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.badum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
